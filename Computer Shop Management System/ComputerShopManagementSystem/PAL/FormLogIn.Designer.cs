6e42f9099e56fedd0d9ee86076f4cbc6f26c8b4f
Mariusz63 <ost.mariusz63@gmail.com>
2024-05-30 11:57:39 +0200

    Create LogIn form

--- Computer Shop Management System/ComputerShopManagementSystem/PAL/FormLogIn.Designer.cs
+++ Computer Shop Management System/ComputerShopManagementSystem/PAL/FormLogIn.Designer.cs
@@ -29,11 +29,223 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogIn));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.picShowPass = new System.Windows.Forms.PictureBox();
+            this.picHidePass = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.picClocse = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.gunaElipse1 = new Guna.UI.WinForms.GunaElipse(this.components);
+            this.gunaElipse2 = new Guna.UI.WinForms.GunaElipse(this.components);
+            this.gunaDragControl1 = new Guna.UI.WinForms.GunaDragControl(this.components);
+            this.guna2HtmlToolTip1 = new Guna.UI2.WinForms.Guna2HtmlToolTip();
+            ((System.ComponentModel.ISupportInitialize)(this.picShowPass)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHidePass)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picClocse)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(82, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Computer Shop Management System | Log In";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label2.Location = new System.Drawing.Point(131, 266);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "User name:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtUserName.Location = new System.Drawing.Point(135, 288);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(238, 21);
+            this.txtUserName.TabIndex = 5;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtPassword.Location = new System.Drawing.Point(135, 345);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(238, 21);
+            this.txtPassword.TabIndex = 7;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label3.Location = new System.Drawing.Point(131, 323);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password:";
+            // 
+            // picShowPass
+            // 
+            this.picShowPass.Image = global::ComputerShopManagementSystem.Properties.Resources._remove_red_eye_90161;
+            this.picShowPass.Location = new System.Drawing.Point(379, 335);
+            this.picShowPass.Name = "picShowPass";
+            this.picShowPass.Size = new System.Drawing.Size(32, 32);
+            this.picShowPass.TabIndex = 9;
+            this.picShowPass.TabStop = false;
+            this.guna2HtmlToolTip1.SetToolTip(this.picShowPass, "Show Password");
+            this.picShowPass.Click += new System.EventHandler(this.picShowPass_Click);
+            // 
+            // picHidePass
+            // 
+            this.picHidePass.Image = global::ComputerShopManagementSystem.Properties.Resources._3844443_disable_eye_inactive_see_show_view_watch_110296;
+            this.picHidePass.Location = new System.Drawing.Point(379, 335);
+            this.picHidePass.Name = "picHidePass";
+            this.picHidePass.Size = new System.Drawing.Size(32, 31);
+            this.picHidePass.TabIndex = 8;
+            this.picHidePass.TabStop = false;
+            this.guna2HtmlToolTip1.SetToolTip(this.picHidePass, "Hide Password");
+            this.picHidePass.Click += new System.EventHandler(this.picHidePass_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(166, 78);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(183, 169);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // picClocse
+            // 
+            this.picClocse.Image = ((System.Drawing.Image)(resources.GetObject("picClocse.Image")));
+            this.picClocse.Location = new System.Drawing.Point(520, 12);
+            this.picClocse.Name = "picClocse";
+            this.picClocse.Size = new System.Drawing.Size(35, 35);
+            this.picClocse.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picClocse.TabIndex = 2;
+            this.picClocse.TabStop = false;
+            this.guna2HtmlToolTip1.SetToolTip(this.picClocse, "Close");
+            this.picClocse.Click += new System.EventHandler(this.picClocse_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(186, 385);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 19);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Forgot Password?";
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnLogIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogIn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnLogIn.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnLogIn.Location = new System.Drawing.Point(176, 420);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(145, 38);
+            this.btnLogIn.TabIndex = 11;
+            this.btnLogIn.Text = "Log In";
+            this.btnLogIn.UseVisualStyleBackColor = false;
+            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
+            // 
+            // gunaElipse1
+            // 
+            this.gunaElipse1.TargetControl = this;
+            // 
+            // gunaElipse2
+            // 
+            this.gunaElipse2.TargetControl = this.btnLogIn;
+            // 
+            // gunaDragControl1
+            // 
+            this.gunaDragControl1.TargetControl = this;
+            // 
+            // guna2HtmlToolTip1
+            // 
+            this.guna2HtmlToolTip1.AllowLinksHandling = true;
+            this.guna2HtmlToolTip1.MaximumSize = new System.Drawing.Size(0, 0);
+            // 
+            // FormLogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormLogIn";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(560, 521);
+            this.Controls.Add(this.btnLogIn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.picShowPass);
+            this.Controls.Add(this.picHidePass);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.picClocse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormLogIn";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Computer Shop Management System | LogIn";
+            ((System.ComponentModel.ISupportInitialize)(this.picShowPass)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHidePass)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picClocse)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picClocse;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox picHidePass;
+        private System.Windows.Forms.PictureBox picShowPass;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnLogIn;
+        private Guna.UI.WinForms.GunaElipse gunaElipse1;
+        private Guna.UI.WinForms.GunaElipse gunaElipse2;
+        private Guna.UI.WinForms.GunaDragControl gunaDragControl1;
+        private Guna.UI2.WinForms.Guna2HtmlToolTip guna2HtmlToolTip1;
     }
 }
--- Computer Shop Management System/ComputerShopManagementSystem/PAL/FormLogIn.cs
+++ Computer Shop Management System/ComputerShopManagementSystem/PAL/FormLogIn.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.Devices;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,66 @@ namespace ComputerShopManagementSystem.PAL
         public FormLogIn()
         {
             InitializeComponent();
+        }
+
+        private void EmptyBox()
+        {
+            txtUserName.Clear();
+            txtPassword.Clear();
+        }
+
+        private void picClocse_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void picShowPass_Click(object sender, EventArgs e)
+        {
+            if(picShowPass.Visible == true)
+            {
+                txtPassword.UseSystemPasswordChar = false;
+                picShowPass.Visible = false;
+                picHidePass.Visible = true;
+            }
+        }
+
+        private void picHidePass_Click(object sender, EventArgs e)
+        {
+            if (picHidePass.Visible == true)
+            {
+                txtPassword.UseSystemPasswordChar = true;
+                picShowPass.Visible = false;
+                picHidePass.Visible = false;
+            }
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            if(txtUserName.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("Please enter user name.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }else if(txtPassword.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("Please enter password", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            else
+            {
+                bool check = Computer.Computer.IsValidNamePass(txtUserName.Text.Trim(), txtPassword.Text.Trim());
+                
+                if(check)
+                {
+                    FormMain formMain = new FormMain();
+                    formMain.ShowDialog();
+                    EmptyBox();
+                }
+                else
+                {
+                    MessageBox.Show("Username or password is incorrect", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+            }
         }
     }
 }
